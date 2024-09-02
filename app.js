352063f85bc8a8b2155f0c53d97f9acac86d9610
Mariia Bohatova <maria.bogatova.01@gmail.com>
2024-09-02 20:01:10 +0300

    start ui

---
+++ app.js
@@ -0,0 +1,16 @@
+let searchBtn = document.querySelector('.header__searchButton');
+let infoBtn = document.querySelector('.header__infoButton');
+let createBtn = document.querySelector('.main__createButton');
+let startPageContent = document.querySelector('.main__emptyContent');
+let headerHeadline = document.querySelector('.header__headline');
+let header = document.querySelector('.header');
+
+
+// createBtn.addEventListener('click', function() {
+//   header.insertAdjacentHTML('beforeend', `
+//     <button class="header__backButton"></button>
+//     <div class="buttonWrapper">
+//       <button class="header__lookButton"></button>
+//       <button class="header__saveButton"></button>
+//     </div>`);
+// })
---
+++ dist/app.dev.js
@@ -0,0 +1,15 @@
+"use strict";
+
+var searchBtn = document.querySelector('.header__searchButton');
+var infoBtn = document.querySelector('.header__infoButton');
+var createBtn = document.querySelector('.main__createButton');
+var startPageContent = document.querySelector('.main__emptyContent');
+var headerHeadline = document.querySelector('.header__headline');
+var header = document.querySelector('.header'); // createBtn.addEventListener('click', function() {
+//   header.insertAdjacentHTML('beforeend', `
+//     <button class="header__backButton"></button>
+//     <div class="buttonWrapper">
+//       <button class="header__lookButton"></button>
+//       <button class="header__saveButton"></button>
+//     </div>`);
+// })
